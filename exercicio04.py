d4efbd25cf4fd737981a316421479cbccc251e2b
Jessica Loris <dearjess535@gmail.com>
2025-04-17 10:26:23 -0300

    salario

---
+++ exercicio04.py
@@ -0,0 +1,5 @@
+salarioBase = 1412
+salarioU = float(input("Digite o valor do seu salário: "))
+CalSalario = salarioU / salarioBase
+
+print(f"Seu salário {salarioU} equivale a {CalSalario} salários minimos.")
