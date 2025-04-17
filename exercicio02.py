00f01d4004606b41993300d2b0f76a7e3ccaf59f
Jessica Loris <dearjess535@gmail.com>
2025-04-17 09:41:00 -0300

    print

--- exercicio02.py
+++ exercicio02.py
@@ -1,12 +1,32 @@
 #Faça um algoritmo para receber um níumero qualquer e imprimir na tela se o número é par ou ímpar,
-# positivo ou negativo
+#positivo ou negativo.
 
-num = int(input("Digite um número: "))
-if num % 2 == 0 and num < 0:
-    print(f"{num} par e negativo")
-elif num == 0 and num >= 0:
-    print(f"{num} impar e positivo")
-elif num != 0 and num < 0:
-    print(f"Seu número {num} é impar e negativo")
+repetir = "S"
+
+while repetir == "S" or repetir == "s":
+    num = int(input("Digite um número: "))
+    if num % 2 == 0 and num < 0:
+        print(f"{num} par e negativo")
+    elif num == 0 and num >= 0:
+        print(f"{num} impar e positivo")
+    elif num != 0 and num < 0:
+        print(f"Seu número {num} é impar e negativo")
+    else:
+        print(f"Seu {num} é impar e positivo")
+    repetir = input("Digite S para continuar po qualquer tecla para parar")
+print("Programa finalizado!")
+
+"""
+Resolução:
+num = int(input("Digite um numero: "))
+if num >0:
+    if num%2==0:
+        print("Par é positivo")
+    else:
+        print("impar e positivo")
 else:
-    print(f"Seu {num} é impar e positivo")
+    if num % 2 ==0:
+        print("par e negativo")
+    else:
+        print("impar e negativo")
+        """
---
+++ exercicio03.py
@@ -0,0 +1,4 @@
+#Faça um algoritmo que receba um número inteiro e imprima na tela o seu numero antecessor e o sucessor
+
+num = int(input("Digite um número: "))
+print(f"{num-1}, {num}, {num+1}")
